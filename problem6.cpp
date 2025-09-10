2c45d148771b0463176a62da81c148f7372b5352
Feruza007-ux <feruzatokhirova07@gmail.com>
2025-09-10 11:50:53 +0500

    Finished the task!

---
+++ problem6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Please enter two integers: "<<endl;
+    cin >> a >> b;
+    cout << "Sum:"<< a<<"+"<<b << "="<< a+b << endl;
+
+    return 0;
+
+}
