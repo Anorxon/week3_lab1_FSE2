f9d96c1ee187809f041472da158c406e6f610567
Feruza007-ux <feruzatokhirova07@gmail.com>
2025-09-10 12:24:42 +0500

    Finished the task!

---
+++ problem2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // This program outputs your name, id, favourite song and movie
+    cout << "Alijon Aliyev " << endl;
+    cout << "233242" << endl;
+    cout << "Queen - The Show Must Go On" <<endl;
+    cout << "Titanic" <<endl;
+    return 0;
+}
