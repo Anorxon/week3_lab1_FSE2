815ef56d78c7a661a5ec47217bf2b1ef6496b55c
Feruza007-ux <feruzatokhirova07@gmail.com>
2025-09-10 13:15:51 +0500

    Finished the task!

---
+++ problem8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cout << "Enter diapeter of sphere : ";
+    cin >> x;
+    float area;
+    area = 4 * 3.14 * x * x;
+    cout << "Area: " << area << endl;
+
+    return 0;
+
+}
