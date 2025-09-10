f98f1686446bebb78bdf60781e7ea7148216b59e
Feruza007-ux <feruzatokhirova07@gmail.com>
2025-09-10 13:03:07 +0500

    Finished the task!

---
+++ problem3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    cout << "\" I'm standing";
+    cout << "\" on the edge of some crazy cliff"<< endl ;
+    cout << "What I have to do , I have to catch everybody " << endl ;
+    cout << "if they start to go over the cliff - I  mean" << endl ;
+    cout << "if they are running and they don't look " << endl ;
+    cout << " where they are going I have to come out"<< endl ;
+    cout << " from where and       catch them "<<endl ;
+    return 0;
+
+}
