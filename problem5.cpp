5a5beca7414cc92d1bcf17c7ffd24028f6013338
Feruza007-ux <feruzatokhirova07@gmail.com>
2025-09-10 13:06:25 +0500

    Finished the task!

---
+++ problem5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, sum , product;
+    cout << "enter two numbers: ";
+    cin >> a >> b;
+    sum = a + b;
+    product = a * b;
+    cout << "sum = " << sum << endl ;
+    cout << "product = " << product << endl ;
+
+    return 0;
+
+}
